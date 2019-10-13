85a6a1898f80f0c242dd44b9037a3f70c6123be2
Romuald Quantin <romu@soundstep.com>
2019-10-13 16:50:04 +0100

    Publish external libs.

--- html.py
+++ html.py
@@ -2,10 +2,36 @@ from pygments import highlight
 from pygments.lexers import HtmlLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<div id="target">{{name}}</div>
-<script type="text/javascript">
-	var target = document.getElementById("target");
-	var template = soma.template.create(target);
+code = """<div class="app">
+  <h1>{{content}}</h1>
+</div>
+
+<script>
+
+  // function model containing data (text to display)
+  var Model = function() {
+    this.data = "Hello world!"
+  };
+
+  // function template that will update the DOM
+  var Template = function(template, scope, element, model) {
+    scope.content = model.data;
+    template.render();
+  };
+
+  // application function
+  var QuickStartApplication = soma.Application.extend({
+    init: function() {
+      // model mapping rule so it can be injected in the template
+      this.injector.mapClass("model", Model, true);
+      // create a template for a specific DOM element
+      this.createTemplate(Template, document.querySelector('.app'));
+    }
+  });
+
+  // create application
+  var app = new QuickStartApplication();
+
 </script>
 """
 
--- update.sh
+++ update.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+# get file from master
+
 rm -rf dist
-git checkout v3 dist/
+git checkout master dist/
 
 rm -rf tests
-git checkout v3 tests/
+git checkout master tests/
 
 rm -rf examples
-git checkout v3 examples/
+git checkout master examples/
+
+# make external libs accessible
+
+rm -rf libs
+mkdir -p libs
+npm install
+cp -f node_modules/@soundstep/infuse/dist/infuse.min.js libs/infuse.min.js
+cp -f node_modules/signals/dist/signals.min.js libs/signals.min.js
