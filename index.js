4d372d8d33e481bd7f413a5a7abfec1d9e093f9a
Romuald Quantin <romu@soundstep.com>
2021-07-31 16:17:40 +0200

    Fix utils.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Commands from './src/application';
 import Emitter from './src/application';
 import Mediators from './src/application';
 import Modules from './src/application';
-import utils from './src/application';
+import utils from './src/utils';
 
 export { Application };
 export { Commands };
